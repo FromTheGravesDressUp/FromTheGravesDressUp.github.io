73aa98762f0b81f61e70b0bdbafe70cba528c5e2
FromTheGravesDressUp <admin@fromthegraves.com>
2024-12-21 21:44:29 -0800

    hue shift test

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
-$(document).ready(function(){
 
-function ImageSwitcher(choices, i) {
-	i = 0;
-	
-	this.Next = function() {
-		hide_current_image();
-		show_next_image();
-	}
-	
-	var hide_current_image = function() {
-		if(choices){
-			choices[i].style.visibility = "hidden";
-			i += 1;
-		}
-	}
-	var show_next_image = function() {
-		if(choices){
-			if(i == (choices.length)) {
-				i = 0;
-			}
-			choices[i].style.visibility = "visible";
-		}
-	}
-}
-  
+$(document).ready(function () {
+    function ImageSwitcher(choices, i) {
+        i = 0;
+
+        this.Next = function () {
+            hide_current_image();
+            show_next_image();
+        }
+
+        var hide_current_image = function () {
+            if (choices) {
+                choices[i].style.visibility = "hidden";
+                i += 1;
+            }
+        }
+        var show_next_image = function () {
+            if (choices) {
+                if (i == (choices.length)) {
+                    i = 0;
+                }
+                choices[i].style.visibility = "visible";
+            }
+        }
+    }
+
     var pants = $(".pant");
-	var shirts = $(".shirt");
-	var backgrounds = $(".bg");
+    var shirts = $(".shirt");
 
-	var shirt_picker = new ImageSwitcher(shirts);
-	document.getElementById("shirt_button").onclick = function() { shirt_picker.Next(); };
-	
-	var pants_picker = new ImageSwitcher(pants);
-	document.getElementById("pant_button").onclick = function() {pants_picker.Next(); };
-	
-	var bg_picker = new ImageSwitcher(backgrounds);
-	document.getElementById("bg_button").onclick = function() {bg_picker.Next(); };
+    var shirt_picker = new ImageSwitcher(shirts);
+    document.getElementById("shirt_button").onclick = function () { shirt_picker.Next(); };
 
+    var pants_picker = new ImageSwitcher(pants);
+    document.getElementById("pant_button").onclick = function () { pants_picker.Next(); };
+
+    // Hue slider functionality for the "shirt" class
+    $("#hue-slider").on("input", function () {
+        let hueValue = $(this).val();
+        $(".shirt").css("filter", `hue-rotate(${hueValue}deg)`);
+    });
 });
-
-  $("#shirt_button").click(function(){
-  $("#shirt-menu").css("visibility", "visible"); });
